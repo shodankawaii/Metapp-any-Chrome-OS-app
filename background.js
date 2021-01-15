adfc9c82e48e81484a0bc498cdc3c8ef9a90ce30
shodankawaii <77482757+shodankawaii@users.noreply.github.com>
2021-01-16 00:56:43 +0200

    Add files via upload

---
+++ background.js
@@ -0,0 +1,10 @@
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.window.create('index.html', {
+    id: 'main',
+    bounds: { width: 620, height: 500 }
+  });
+  
+  chrome.app.runtime.onLaunched.addListener(function() {
+  //...
+});
+});
